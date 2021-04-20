9a0b452e8f31babff75bf130d55cacd7a9733412
Akash Kumar <53407198+biharihu@users.noreply.github.com>
2021-04-20 13:43:14 +0530

    added avatar upload

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,24 +1,10 @@
 const express = require("express");
 require("./db/mongoose");
-
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
-
-// app.use((req, res, next) => {
-//   if (req.method === "GET") {
-//     res.send("Get requests are disabled");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send("Site is currently down. Check back soon!");
-// });
 
 app.use(express.json());
 app.use(userRouter);
@@ -27,18 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const Task = require("./models/task");
-const User = require("./models/user");
-
-const main = async () => {
-  // const task = await Task.findById("607d79737c8c6826a85c3f70");
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-
-  const user = await User.findById("607d78bd85643226584e720f");
-  await user.populate("tasks").execPopulate();
-  console.log(user.tasks);
-};
-
-main();
